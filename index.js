705fd51b043a0a58dcdce65e703204f3a6a8ad2b
iamso <so@dev.so>
2016-09-16 01:09:41 +0200

    add IIFE wrapper and strict mode

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-const fs = require('fs');
-const fse = require('fs-extra');
-const rimraf = require('rimraf');
-const Zip = require('adm-zip');
-const uuid = require('node-uuid');
+(function() {
+  'use strict';
 
-const strapboot = require('strapboot');
+  const fs = require('fs');
+  const fse = require('fs-extra');
+  const rimraf = require('rimraf');
+  const Zip = require('adm-zip');
+  const uuid = require('node-uuid');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs        = require('ejs');
+  const strapboot = require('strapboot');
 
-const port = process.env.PORT || 6789;
-const encoding = 'utf-8';
-const base = `${__dirname}/temp`;
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const ejs        = require('ejs');
 
-let app = express();
+  const port = process.env.PORT || 6789;
+  const encoding = 'utf-8';
+  const base = `${__dirname}/temp`;
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.text({ type: 'text/html' }));
+  let app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
-});
+  app.use(express.static(__dirname + '/public'));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.text({ type: 'text/html' }));
 
-app.post('/', (req, res) => {
-  let answers = req.body;
-  let id = uuid.v4();
-  let dir = `${base}/${id}`;
-  let files = new Zip();
-  strapboot(answers, dir).then(() => {
-    files.addLocalFolder(dir);
-    files = files.toBuffer();
-    res.setHeader('Content-disposition', 'attachment; filename=strapboot.zip');
-    res.setHeader('Content-type', 'application/zip');
-    res.send(files);
-    rimraf.sync(dir);
+  app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
   });
-});
 
-let server = app.listen(port, () => {
-  console.log('Listening on port %d', server.address().port);
-});
+  app.post('/', (req, res) => {
+    let answers = req.body;
+    let id = uuid.v4();
+    let dir = `${base}/${id}`;
+    let files = new Zip();
+    strapboot(answers, dir).then(() => {
+      files.addLocalFolder(dir);
+      files = files.toBuffer();
+      res.setHeader('Content-disposition', 'attachment; filename=strapboot.zip');
+      res.setHeader('Content-type', 'application/zip');
+      res.send(files);
+      rimraf.sync(dir);
+    });
+  });
+
+  let server = app.listen(port, () => {
+    console.log('Listening on port %d', server.address().port);
+  });
+
+})();
